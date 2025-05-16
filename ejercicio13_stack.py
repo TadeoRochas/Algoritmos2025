4d55e2e1b2e35f1d9c8f39a63941e6d96211c213
TadeoRochas <tadeorochas05@gmail.com>
2025-05-16 01:14:09 -0300

    Add files via upload

--- ejercicio13_stack.py
+++ ejercicio13_stack.py
@@ -3,7 +3,10 @@ stack_trajes = [
     {"modelo": "Mark III", "pelicula": "Iron Man", "estado": "Dañado"},
     {"modelo": "Mark XLIV", "pelicula": "Avengers: Age of Ultron", "estado": "Impecable"},
     {"modelo": "Mark L", "pelicula": "Avengers: Infinity War", "estado": "Destruido"},
-    {"modelo": "Mark XL", "pelicula": "Iron Man 3", "estado": "Destruido"}]
+    {"modelo": "Mark XL", "pelicula": "Iron Man 3", "estado": "Destruido"},
+    {"modelo": "XLVII" , "pelicula": "Spiderman: Homecoming", "estado": "Dañado"},
+    {"modelo": "Mark XLVI", "pelicula": "Captain America: Civil War", "estado": "Impecable"},
+    ]
 
 trajes_destruidos = []
 
@@ -37,8 +40,22 @@ def eliminarDestruidos(stack_trajes):
             trajesDestruidos.append(traje["modelo"])
             stack_trajes.remove(traje)
     return print(f"Los trajes destruidos fueron: {trajesDestruidos}")
+
+def mostrarTrajes(stack_trajes):
+    trajes_pelis = []
+    for traje in stack_trajes:
+        if traje["pelicula"] == "Spiderman: Homecoming" or traje["pelicula"] == "Capitan America: Civil War":
+             trajes_pelis.append(traje["modelo"])
+    return trajes_pelis
             
+def agregarTraje(stack_trajes, modelo, pelicula, estado):
+    nuevo_traje = {"modelo": modelo, "pelicula": pelicula, "estado": estado}
+    stack_trajes.append(nuevo_traje)
+    return stack_trajes
+
 print(buscar_traje(stack_trajes, "Mark XLIV"))
 print(mostrarDañados(stack_trajes))
 print(eliminarDestruidos(stack_trajes))
 print(stack_trajes)
+print(agregarTraje(stack_trajes, "Mark LXXXVI", "Avengers: Endgame", "Impecable"))
+print(mostrarTrajes(stack_trajes))
